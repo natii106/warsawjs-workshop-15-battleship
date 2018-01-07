b86c0ed3e44ec83875218bd023db39ea15ecc110
Natalia Walasik <natalia-walasik@wp.pl>
2018-01-07 12:39:36 +0100

    Add a clickable cell that change its status

--- game.js
+++ game.js
@@ -1,26 +1,35 @@
-const gameContainer = document.querySelector('#game');
-const cell = document.createElement('div');
-//const niezmienialny, let modyfikowalny
-//cell.innerText depricated
-//const textNode = document.createTextNode('Hello, world');
-//cell.appendChild(textNode); same as textContent, shows basics of the structure
-cell.textContent = 'Hello, world!';
-gameContainer.appendChild(cell);
-
-let onClick = function onClick() {
-  cell.textContent = 'Clicked';
+'use strict';
+class Component {
+  getElement() {
+    return this._element;
+  }
 }
-cell.addEventListener('click', onClick;
 
-//1. ma ona name i mozna ja pobrac, bedzie w stack trace
-//2. wykona sie za kazdym razem
-// bedzie w function scope
-// //to jest to samo co var onCLick =
-// function onClick() {
-//   console.log('');
-// }
-// // funkcja anonimowa
-// //temporal dead zone dla let i const
-// const onCLick2 = function() {
-//   console.log('2');
-// }
+class CellComponent extends Component {
+  constructor() {
+    super();
+    //this dopiero po super
+    //podkreslnik to konwencja na zmienne prywatne
+    this._state = 'unknown';
+    this._element = document.createElement('td');
+    this._element.addEventListener('click', function() {
+      this.setState('miss');
+    }.bind(this));
+    this._refresh();
+  }
+
+  setState(stateName) {
+    this._state = stateName;
+    this._refresh();
+  }
+
+  _refresh() {
+    this._element.textContent = this._state;
+    this._element.className = 'cell_' + this._state;
+  }
+}
+
+const myCell = new CellComponent();
+document
+  .getElementById('cellContainer')
+  .appendChild(myCell.getElement());
